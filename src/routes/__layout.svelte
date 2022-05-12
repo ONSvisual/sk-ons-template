b4a0ad5e9ac0f5edea0e3fa039008785fbd55bd8
Ahmad Barclay <abarclay.ons@gmail.com>
2022-05-12 13:56:24 +0100

    fixes to cookie banner

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,7 +7,7 @@
 	import Warning from "$lib/ui/Warning.svelte";
 	import ONSHeader from "$lib/layout/ONSHeader.svelte";
 	import ONSFooter from "$lib/layout/ONSFooter.svelte";
-  import CookieBanner from "$lib/layout/CookieBanner.svelte";
+  import AnalyticsBanner from "$lib/layout/AnalyticsBanner.svelte";
 	
   // For localisation of menu etc
   let path = $page.url.pathname;
@@ -20,39 +20,9 @@
   setContext("theme", themes[theme]);
 
   // GOOGLE ANALYTICS
-  // Settings for page analytics. usageCookies needs to be bound to the <CookieBanner> component
-  // You need to define product-specific analyticsProps in config.js
-  const live = $page.url.hostname == "www.ons.gov.uk" || $page.url.hostname == "cy.ons.gov.uk";
-  let usageCookies;
-
-  function initAnalytics() {
-    window.dataLayer = [{
-      "analyticsOptOut": false,
-      "gtm.whitelist": ["google","hjtc","lcl"],
-      "gtm.blacklist": ["customScripts","sp","adm","awct","k","d","j"],
-      ...analyticsProps
-    }];
-    window.gtag = function gtag() {
-      window.dataLayer.push(arguments);
-    };
-    window.gtag("js", new Date());
-    window.gtag("config", analyticsId);
-  };
-
-  // This code should only be included in multi-page apps. It sends an analytics event when the URL changes
-  $: if (typeof gtag !== 'undefined') {
-    if ($page.url.searchParams.toString().length > 0) {
-      gtag('config', analyticsId, {
-        page_path : $page.url.pathname,
-        page_location: `${$page.url.hostname}${$page.url.pathname}?${$page.url.searchParams}`,
-      });
-    } else {
-      gtag('config', analyticsId, {
-        page_path : $page.url.pathname,
-        page_location: `${$page.url.hostname}${$page.url.pathname}`,
-      });
-    }
-  }
+  // Settings for page analytics. Values must be shared with <AnalyticsBanner> component
+  // 'analyticsId' and 'analyticsProps' are defined in config.js
+  let gtag;
 </script>
 
 <svelte:head>
@@ -61,21 +31,20 @@
 	<meta property="og:url" content="{base}/" />
 	<meta property="og:image" content="{base}/img/og.png" />
 	<meta property="og:image:type" content="image/png" />
-  {#if usageCookies && live}
-  <script async src="https://www.googletagmanager.com/gtag/js?id={analyticsId}" on:load={initAnalytics}></script>
-  {/if}
 </svelte:head>
 
 <Warning>
   WARNING! This is a prototype.
 </Warning>
 
-<CookieBanner bind:usageCookies/>
+{#if false}
+<AnalyticsBanner {analyticsId} {analyticsProps} {page} bind:gtag/>
+{/if}
 
 <ONSHeader {baseurl} {path} {lang}/>
 
 <main id="main" tabindex="-1">
-  <slot/>
+  <slot {gtag}/>
 </main>
 
 <ONSFooter {baseurl} {lang}/>
